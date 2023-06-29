ac7352a92fb01bfc03ec21b83a78eff91d0a9b41
satelightccb <crazy_soci80@yahoo.co.jp>
2023-06-29 21:58:05 +0900

    mac だと動かないのでここまで

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 //!FIBAR
 //! File Backet Relay
-//! this app is sending and web api server.
-//!
+//! This app is sending and web api server.
+//! This app is adapted from  window xp to window 10.
+//! This code used for rust version 1.49.
+//! Latest rust version don't working.
+//! https://doc.rust-jp.rs/book-ja/ch20-01-single-threaded.html
 
+use std::fs::File;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -19,8 +23,23 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let mut file = File::open(filename).unwrap();
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
